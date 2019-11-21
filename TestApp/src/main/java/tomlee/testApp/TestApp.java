74c3abdd5362810dbcf7a126df0b43f0984869c8
Tom <tomlee22@gmail.com>
2019-11-20 20:40:31 -0800

    Initial commit

---
+++ TestApp/src/main/java/tomlee/testApp/TestApp.java
@@ -0,0 +1,10 @@
+package tomlee.testApp;
+
+
+import com.google.common.collect.ImmutableList;
+
+public class TestApp {
+    public static void main(String[] args) {
+        System.out.println(ImmutableList.of(3,2,1));
+    }
+}
